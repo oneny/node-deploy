38124a6ba4c7eb00ea7b1b98aaf5d70be7d5ad18
oneny <gjwodnjs1996@gmail.com>
2022-04-18 08:54:20 +0900

    S3 upload arragement

--- routes/middlewares.js
+++ routes/middlewares.js
@@ -1,3 +1,5 @@
+const RateLimit = require('express-rate-limit');
+
 exports.isLoggedIn = (req, res, next) => {
   if (req.isAuthenticated()) {
     next();
@@ -14,3 +16,14 @@ exports.isNotLoggedIn = (req, res, next) => {
     res.redirect(`/?error=${message}`);
   }
 };
+
+exports.apiLimiter = RateLimit({
+  windowMs: 60 * 1000, // 1분
+  max: 3,
+  handler(req, res) {
+    res.status(this.statusCode).json({
+      code: this.statusCode, // 기본값 429
+      message: '1분에 3번만 요청할 수 있습니다!',
+    });
+  }
+});
--- routes/post.js
+++ routes/post.js
@@ -2,6 +2,8 @@ const express = require('express');
 const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
+const AWS = require('aws-sdk');
+const multerS3 = require('multer-s3');
 
 const { Post, Hashtag } = require('../models');
 const { isLoggedIn } = require('./middlewares');
@@ -15,14 +17,17 @@ try {
   fs.mkdirSync('uploads');
 }
 
+AWS.config.update({
+  accessKeyId: process.env.S3_ACCESS_KEY_ID,
+  secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+  region: 'ap-northeast-2',
+});
 const upload = multer({
-  storage: multer.diskStorage({
-    destination(req, file, cb) {
-      cb(null, 'uploads/');
-    },
-    filename(req, file, cb) {
-      const ext = path.extname(file.originalname);
-      cb(null, path.basename(file.originalname, ext) + Date.now() + ext);
+  storage: multerS3({
+    s3: new AWS.S3(),
+    bucket: 'nodebird96',
+    key(req, file, cb) {
+      cb(null, `original/${Date.now()}${path.basename(file.originalname)}`);
     },
   }),
   limits: { fileSize: 5 * 1024 * 1024 }
@@ -30,8 +35,7 @@ const upload = multer({
 
 router.post('/img', isLoggedIn, upload.single('img'), (req, res) => {
   console.log(req.file); // 업로드한 결과물이 req.file에 저장됨
-  res.json({ url: `/img/${req.file.filename}` });
-  // 실제 파일은 uploads 폴더 안에 있는데 요청 주소는 /img
+  res.json({ url, originalUrl });
 });
 
 const upload2 = multer();
